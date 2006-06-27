32ff2615b99d6a8c6a9568830d6243f3ba0c88ba
Gundalf <gundalf01@users.sourceforge.net>
2006-06-27 11:27:14 +0000

    basic placing of markers

--- NifFurniture/resource.h
+++ NifFurniture/resource.h
@@ -1,23 +1,20 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
+// Microsoft Visual C++ generated include file.
 // Used by NifFurniture.rc
 //
-
-
 #define IDS_LIBDESCRIPTION              1
 #define IDS_CATEGORY                    2
 #define IDS_CLASS_NAME                  3
 #define IDS_PARAMS                      4
 #define IDS_SPIN                        5
-
-
-
+#define IDS_FURNITURE_MARKER            6
 #define IDD_PANEL                       101
-
 #define IDC_CLOSEBUTTON                 1000
 #define IDC_DOSTUFF                     1000
+#define IDC_CB_TYPE                     1001
 #define IDC_COLOR                       1456
 #define IDC_EDIT                        1490
+#define IDC_EDIT2                       1491
 #define IDC_SPIN                        1496
 
 // Next default values for new objects
@@ -26,7 +23,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
