9fcffbe7e96055d84724fc65a45fa6ab6a222d52
Tazpn <tazpn@users.sourceforge.net>
2008-10-07 04:29:40 +0000

    max: Incorporate build numbers into version strings

--- NifCommon/NifVersion.h
+++ NifCommon/NifVersion.h
@@ -13,17 +13,28 @@ HISTORY:
 #ifndef __NIFVERSION_H__
 #define __NIFVERSION_H__
 
+#include "config.h"
+
 /*
 * Define the major, minor, build, patch version numbers
 */
 #define VERSION_MAJOR_INT  3
 #define VERSION_MINOR_INT  0
 #define VERSION_BUILD_INT  5
+
+#ifndef NIFTOOLS_REVISION_INT
 #define VERSION_PATCH_INT  0
+#else
+#define  VERSION_PATCH_INT  NIFTOOLS_REVISION_INT
+#endif
 
+#ifndef NIFTOOLS_REVISION
 #define VERSION_STRING "3, 0, 5, 0"
+#else
+#define VERSION_STRING "3, 0, 5, " NIFTOOLS_REVISION
+#endif
 
-//#define DEF_VERSION_STRING(a,b,c,d) #a ", " #b ", " #c ", " #d
+//#define DEF_VERSION_STRING(a,b,c,d) "##a, ##b , ##c , ##d"
 //#define VERSION_STRING DEF_VERSION_STRING(a,b,c,d)
 
 #define COPYRIGHTYEARINT 2008
