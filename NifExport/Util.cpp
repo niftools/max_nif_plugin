7051d1195d8390cf836f69a9f8f35b7f79585cf5
Tazpn <tazpn@users.sourceforge.net>
2008-10-08 02:45:49 +0000

    max: revert unnecessary changes

--- NifExport/Util.cpp
+++ NifExport/Util.cpp
@@ -137,10 +137,7 @@ NiNodeRef Exporter::getNode(INode* maxNode)
 	NodeToNodeMap::iterator itr = mNodeMap.find(maxNode);
 	if (itr != mNodeMap.end())
 		return (*itr).second;
-	//NiNodeRef node = getNode(name);
-	NiNodeRef node = CreateNiObject<NiNode>();
-	node->SetName(name);
-	mNameMap[name] = node;
+	NiNodeRef node = getNode(name);
 	mNodeMap[maxNode] = node;
 	return node;
 }
@@ -158,17 +155,8 @@ NiNodeRef Exporter::getNode(const string& name)
 NiNodeRef Exporter::makeNode(NiNodeRef &parent, INode *maxNode, bool local)
 {
 	string name = (char*)maxNode->GetName();
+	NiNodeRef node = getNode(maxNode);
 
-	// Leave now if node has already been seen
-	NodeToNodeMap::iterator itr = mNodeMap.find(maxNode);
-	if (itr != mNodeMap.end())
-		return (*itr).second;
-
-	NiNodeRef node = CreateNiObject<NiNode>();
-	node->SetName(name);
-	mNodeMap[maxNode] = mNameMap[name] = node;
-
-	// apply scale now?
 	Matrix33 rot;
 	Vector3 trans;
 	TimeValue t = 0;
