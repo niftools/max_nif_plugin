89878c57af143ede6e1189a925ea2c7650c9ec4e
Tazpn <tazpn@users.sourceforge.net>
2008-10-19 22:50:21 +0000

    max:  Add default config.h but try also add ignore to nifcommon for config.h

--- makeconfig.bat
+++ makeconfig.bat
@@ -2,6 +2,4 @@
 rem Quick script to create config.h
 IF EXIST "%PROGRAMFILES%\TortoiseSVN\bin\SubWCRev.exe" (
     "%PROGRAMFILES%\TortoiseSVN\bin\SubWCRev.exe" . NifCommon\config.h.in NifCommon\config.h > nul 2<&1
-) else (
-    copy NifCommon\config.h.in NifCommon\config.h
 )
